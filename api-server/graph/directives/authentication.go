23568b96b9eb15ece575ee04a836c1e3c4f8183a
David van der Spek <vanderspek.david@gmail.com>
2023-03-30 15:59:49 +0200

    enabled authenticated directive
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- api-server/graph/directives/authentication.go
+++ api-server/graph/directives/authentication.go
@@ -2,31 +2,34 @@ package directives
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/pluralsh/oauth-playground/api-server/handlers"
 )
 
 func (d *Directive) IsAuthenticated(ctx context.Context, obj interface{}, next graphql.Resolver) (interface{}, error) {
-	// userCtx := auth.ForContext(ctx)
-	// if userCtx != nil {
+	userCtx := handlers.ForContext(ctx)
+	if userCtx != nil {
 
-	// 	if userCtx.KratosSession != nil {
+		if userCtx.KratosSession != nil {
 
-	// 		session := userCtx.KratosSession
+			session := userCtx.KratosSession
 
-	// 		if !session.GetActive() || session.ExpiresAt.Before(time.Now()) {
-	// 			return nil, fmt.Errorf("Access denied: User session not active or has expired.")
-	// 		}
-	// 	} else {
-	// 		// TODO: remove debug log message
-	// 		setupLog.Info("auth directive failed")
-	// 		return nil, fmt.Errorf("Access denied: User must be logged in.")
-	// 	}
-	// } else {
-	// 	// TODO: remove debug log message
-	// 	setupLog.Info("auth directive failed")
-	// 	return nil, fmt.Errorf("Access denied: User must be logged in.")
-	// }
+			if !session.GetActive() || session.ExpiresAt.Before(time.Now()) {
+				return nil, fmt.Errorf("Access denied: User session not active or has expired.")
+			}
+		} else {
+			// TODO: remove debug log message
+			d.C.Log.Info("auth directive failed")
+			return nil, fmt.Errorf("Access denied: User must be logged in.")
+		}
+	} else {
+		// TODO: remove debug log message
+		d.C.Log.Info("auth directive failed")
+		return nil, fmt.Errorf("Access denied: User must be logged in.")
+	}
 
 	// TODO: remove debug log message
 	// setupLog.Info("auth directive successful")
